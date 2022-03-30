bb83f5c5b62a32f3d466b01ed78e4dec560c9c78
nidhij-addweb <nidhi.j.addweb@gmail.com>
2022-03-30 10:11:41 +0530

    j

--- march-16/php-local-variables/localvariable.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo"<h3>LOCAL VARIABLES-PHP</h3>";
-echo"<br>";
-function localvariable(){
-    $text="hello";
-    echo"local variable : ".$text;
-}
-localvariable();
-?>
--- march-16/php-query-string/action.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo" <h3>QUERY STRING-PHP</h3>";
-$name=$_GET['name'];
-$email=$_GET['email'];
-echo"<h4>name is $name <br> email is $email</h4>";
-
-?>
