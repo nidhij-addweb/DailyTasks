767e090d0f02cdb695f599ece71260b92161969a
nidhij-addweb <nidhi.j.addweb@gmail.com>
2022-03-30 10:13:41 +0530

    j

--- march 7/28.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo"Program to check whether a number is Even or Odd. (Using ? : operator)";
-echo"<br>";
-$num=6;
-echo ($num%2==0)?"even number":"odd number";
-?>
--- march 7/3.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo "Program to calculate the area of a square while the perimeter is given.";
-echo "<br>";
-$p=64;
-$a=($p*$p)/16;
-echo "area of square ".$a;
-
-?>
